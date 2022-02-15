c12b35cbc8f8ef3fa53fda94896c75df3c0d4492
AlwinW <16846521+AlwinW@users.noreply.github.com>
2022-02-15 17:51:05 +1100

    Convert dstoute classes

--- Python/src/pyJanus.cpp
+++ Python/src/pyJanus.cpp
@@ -22,97 +22,65 @@
 // SOFTWARE.
 //
 
+#include <pybind11/iostream.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
 
 #include <Janus/Janus.h>
+#include <Ute/aString.h>
+
+#include "pyrepr.hpp"
+
+using namespace janus;
+using namespace dstoute;
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-struct Document
-{
-    Document(const std::string &name, int pages) : name(name), pages(pages) {}
-
-    void setName(const std::string &name_) { name = name_; }
-    const std::string &getName() const { return name; }
-
-    void set(int pages_) { pages = pages_; }
-    void set(const std::string &name_) { name = name_; }
-
-    int add(int i, int j = 2)
-    {
-        return i + j;
-    }
-
-    std::string name;
-    int pages;
-};
-
-struct Book : Document
-{
-    Book(const std::string &name, int pages) : Document(name, pages) {}
-    std::string flick() const { return "next page!"; }
-};
-
-using namespace janus;
 namespace py = pybind11;
+
+template <typename... Args>
+using overload_cast_ = py::detail::overload_cast_impl<Args...>;
 
 PYBIND11_MODULE(pyJanus, m)
 {
-    py::class_<Document>(m, "Document")
-        .def(py::init<const std::string &, int>(), py::arg("name"), py::arg("pages"))
-        .def_property("name", &Document::getName, &Document::setName)
-        .def("set", static_cast<void (Document::*)(int)>(&Document::set), "Set the Document's pages")
-        .def("set", static_cast<void (Document::*)(const std::string &)>(&Document::set), "Set the Document's name")
-        .def("add", &Document::add, "A function which adds two numbers",
-             py::arg("i"), py::arg("j") = 2)
-        .def("__repr__",
-             [](const Document &a)
-             {
-                 return "<pyJanus.Document named '" + a.name + "'>";
-             });
-
-    py::class_<Book, Document>(m, "Book")
-        .def(py::init<const std::string &, int>(), py::arg("name"), py::arg("pages"))
-        .def("flick", &Book::flick)
-        .def("__repr__",
-             [](const Document &a)
-             {
-                 return "<pyJanus.Book named '" + a.name + "'>";
-             });
-
-    m.def("library", []()
-          { return std::unique_ptr<Document>(new Book("xml", 10)); });
+    py::class_<aString>(m, "aString")
+        .def(py::init<const std::string &>());
+    py::implicitly_convertible<std::string, aString>();
 
     py::class_<VariableDef>(m, "VariableDef")
-    // .def(py::init<const std::string &>()), py::arg()
-    // .def("setValue", &setValue);
-    .def("__repr__",
-             [](const VariableDef &a)
+        // .def(py::init<const std::string &>()), py::arg()
+        // .def("setValue", &setValue);
+        .def("__repr__",
+             [](const VariableDef &self)
              {
-                 return "<pyJanus.VariableDef('')>";
+                 std::ostringstream out;
+                 out << "VariableDef("
+                     << "name='" << self.getName() << "', "
+                     << "varID='" << self.getVarID() << "', "
+                     << "units='" << self.getUnits() << "', "
+                     << "initialValue='" << self.getInitialValue() << "'"
+                     << ")";
+                 return out.str();
              });
+
+    // <variableDef name="angleOfAttack" varID="angleOfAttack" units="deg" initialValue="0.0">
 
     py::class_<Janus>(m, "Janus")
         .def(py::init<const std::string &>(), py::arg("filename"))
         .def("get_variabledef",
-             static_cast<VariableDefList& (Janus::*)()>(&Janus::getVariableDef))
+             static_cast<VariableDefList &(Janus::*)()>(&Janus::getVariableDef))
         .def("get_variabledef",
-             static_cast<VariableDef& (Janus::*)(dstoute::aOptionalSizeT)>(&Janus::getVariableDef))
+             static_cast<VariableDef &(Janus::*)(const aString &)>(&Janus::getVariableDef))
         .def_property("xml_filename", &Janus::getXmlFileName, nullptr)
+        .def("aString", [](const std::string &s)
+             { return s; })
         .def("__repr__",
-             [](const Janus &a)
+             [](const Janus &self)
              {
-                 return "<pyJanus.Janus('" + a.getXmlFileName() + "')>";
+                 return "<pyJanus.Janus('" + self.getXmlFileName() + "')>";
              });
-
-// cmake --build build -j8 --config Release
-// cd build/Python/Release
-//
-// import pyJanus
-// j = pyJanus.Janus("../../../Examples/CombinedExample.xml")
-// j.get_variabledef()
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
@@ -120,3 +88,14 @@ PYBIND11_MODULE(pyJanus, m)
     m.attr("__version__") = "dev";
 #endif
 }
+
+// cmake . -B build -DBUILD_PYBINDINGS=ON -DPY_VERSION_INFO=0.0.1
+// cmake --build build -j8 --config Release
+//
+// cd build/Python/Release
+//
+// ipython
+// import pyJanus
+// j = pyJanus.Janus("../../../Examples/CombinedExample.xml")
+// j.get_variabledef()
+// j.get_variabledef("angleOfAttack")
